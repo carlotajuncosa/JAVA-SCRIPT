aa512255f4f19274844f9476c1c6d482cb65f6be
carlotajuncosa <carlota.juncosa@gmail.com>
2022-10-04 09:20:16 +0200

    Iteration#2.js

--- Iteration#2.js
+++ Iteration#2.js
@@ -16,6 +16,7 @@ console.log(character)
 ambos juguetes. */
 const toy1 = {name: 'Buss myYear', price: 19};
 const toy2 = {name: 'Rallo mcKing', price: 29};
+ console.log(toy1.price + toy2.price);
 
 /* 1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000 
 y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad 
@@ -23,3 +24,9 @@ basePrice más el valor de la variable globalBasePrice. */
 let globalBasePrice = 10000;
 const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
 const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+
+globalBasePrice = 25000;
+car1.basePrice = 25000;
+car2.basePrice = 25000;
+
+console.log(car1, car2);
