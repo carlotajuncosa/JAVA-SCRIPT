90c09d3ba9c27b4cab43bb87331ebfc030908053
carlotajuncosa <carlota.juncosa@gmail.com>
2022-10-04 09:34:42 +0200

    End Iterations #1#2#3

--- Iteration#3.js
+++ Iteration#3.js
@@ -23,3 +23,8 @@ console.log(x);
 
 /* 1.5 Usa el correcto operador de asignación que resultará en x = 50,
 teniendo dos variables y = 10 y z = 5. */
+
+var y = 10;
+var z = 5;
+var x = z * y;
+console.log(x);
