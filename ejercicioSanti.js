55a4e8a77fd5707bb42089d31bf0a015a4c7e025
carlotajuncosa <carlota.juncosa@gmail.com>
2022-10-04 12:13:23 +0200

    END block1

---
+++ ejercicioSanti.js
@@ -0,0 +1,18 @@
+//Calculadora de edad
+/* año de nacimiento
+año actual
+obtener los años de la persona
+
+Queremos hacer una calculadora de edad, para ello necesitamos dos variables, 
+la primera será el año de nacimiento de la persona de la que queramos 
+calcular la edad y la segunda será el año actual (podemos conseguirla por 
+javascript o si no sabemos escribimos el año a mano) y tenemos que hacer 
+cierta operación para conseguir adivinar cuantos años tiene esa persona
+en la actualidad */
+
+var nacimiento = 1984;
+var anioActual = 2022;
+
+var edad = anioActual - nacimiento;
+
+console.log(edad);
