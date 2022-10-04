cb8b4baa477248d5f8f193fe951d9a0d6541b7db
carlotajuncosa <carlota.juncosa@gmail.com>
2022-10-04 08:49:04 +0200

    FEAT apuntes and basic algorithms

---
+++ apuntes/index.js
@@ -0,0 +1,43 @@
+/* console.log('esto impresion');
+
+const div = documnet.getElementById('test');
+console.log('DIV CAPTURADO', testDiv);
+ */
+/* alert ('hola esto es un alert') */
+
+/* console.log(process); */
+
+/* console.log('hola');
+
+/* var= 'adios'; 
+
+function trolololo(){
+var= 'hola';
+
+}
+if (true){
+
+} */
+
+var bote;
+
+console.log(bote);
+
+//no es necesario poner var porque ya la hemos declarado antes
+
+bote = 'spaguettis';
+console.log(bote);
+bote= 1532;
+console.log(bote);
+
+var boteNumerico = 288;
+
+var boteInterruptor = true;
+
+var boteObjeto = {
+    nombre: 'álvaro',
+    apellido: 'Moreno',
+}
+//get y set --- getters y setters -- obtener y grabar
+console.log(bote);
+
