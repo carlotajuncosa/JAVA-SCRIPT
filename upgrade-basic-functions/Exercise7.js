6b066a40c6842e20800432f1246d47d114995fa4
carlotajuncosa <carlota.juncosa@gmail.com>
2022-10-07 18:58:28 +0200

    Exercise7.js solved

--- upgrade-basic-functions/Exercise7.js
+++ upgrade-basic-functions/Exercise7.js
@@ -4,22 +4,44 @@ Crea una función que reciba por parámetro un array y el valor
 que desea comprobar que existe dentro de dicho array - comprueba 
 si existe el elemento, en caso que existan nos devuelve la 
 osición de dicho elemento y por la contra un false. Puedes usar 
-este array para probar tu función: */
+este array para probar tu función:
+
+/* función( array, valor a comprobar)
+
+  for (const element of array){
+    if (element === valor a comprobar) {
+      existe
+      console.log(array[i] + valor a comprobar);
+    } else {
+      no existe
+      console.log(false);
+    }
+  }*/
 
 const nameFinder = [
-    'Peter',
-    'Steve',
-    'Tony',
-    'Natasha',
-    'Clint',
-    'Logan',
-    'Xabier',
-    'Bruce',
-    'Peggy',
-    'Jessica',
-    'Marc'
-  ];
-  function finderName(param) {
-    // insert code
-  }
+  "Peter",
+  "Steve",
+  "Tony",
+  "Natasha",
+  "Clint",
+  "Logan",
+  "Xabier",
+  "Bruce",
+  "Peggy",
+  "Jessica",
+  "Marc",
+];
+function finderName(array, key) {
+  // key = valor a comprobar
 
+  for (var i = 0; i < array.length; i++) {
+    //console.log('Estoy recorriendo los elementos del array: ' + nameFinder[i]);
+
+    if (array[i] === key) {
+      return `${i}: ${array[i]}`;
+    }
+  }
+  return false;
+}
+
+console.log(finderName(nameFinder, "Peggy"));
