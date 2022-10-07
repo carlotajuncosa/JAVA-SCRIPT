fd06d066cb5bd8f8b125b25b319f6707c8e0f50a
carlotajuncosa <carlota.juncosa@gmail.com>
2022-10-07 21:05:17 +0200

    Exercise8.js

--- upgrade-basic-functions/Exercise8.js
+++ upgrade-basic-functions/Exercise8.js
@@ -4,6 +4,13 @@ Crea una función que nos devuelva el número de veces que se repite
 cada una de las palabras que lo conforma.  Puedes usar este array 
 para probar tu función: */
 
+//recorrer el loop 
+//condicional i++ si la palabra se repite el string.
+//
+//poner dentro de una funcion
+//console log tiene que aparecer '' + numero de vecesque aparece 
+
+
 const counterWords = [
     'code',
     'repeat',
@@ -17,6 +24,13 @@ const counterWords = [
     'upgrade',
     'code'
   ];
-  function repeatCounter(param) {
+  //function repeatCounter(param) {
     // insert code
+  //}
+
+  for (var i = 0; i < counterWords.length; i++) {
+    //console.log(i)
+    if ( counterWords[i] === counterWords[i]) {
+      console.log(++i, counterWords[i])
+    }
   }
