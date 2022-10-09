9a8f38c6baaed67f8d6f5d6666f242d2a4d928ab
carlotajuncosa <carlota.juncosa@gmail.com>
2022-10-09 18:23:20 +0200

    initial commit Iteration 6 and 7 loops

--- upgrade-loops/Iteration#6.js
+++ upgrade-loops/Iteration#6.js
@@ -5,10 +5,30 @@ juguetes y elimina los que incluyan la palabra gato.
 Recuerda que puedes usar la función ***.includes()*** 
 para comprobarlo.Puedes usar este array: */
 
-const toys1 = [
+const toys = [
     {id: 5, name: 'Buzz MyYear'}, 
     {id: 11, name: 'Action Woman'}, 
     {id: 23, name: 'Barbie Man'}, 
     {id: 40, name: 'El gato con Guantes'},
     {id: 40, name: 'El gato felix'}
     ]
+
+
+    for (let i = 0; i < toys.length; i++) {
+        if (toys[i]['name'] == 'gato' ||
+        toys[i]['name'] == 'gato' ) {
+          toys.splice(i,1);
+          i--;
+        }
+      } 
+
+      console.log(toys);
+          
+
+
+      /* if (array[i].includes('Camiseta')){
+        console.log(array[i])
+        }
+    }    
+}
+camisetas(products); */
