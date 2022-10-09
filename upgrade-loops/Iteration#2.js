78f09eeda1e3ace50d2fa9f42e000502da3119fe
carlotajuncosa <carlota.juncosa@gmail.com>
2022-10-09 13:09:02 +0200

    Iteration 2 loops with conditional and ternary

--- upgrade-loops/Iteration#2.js
+++ upgrade-loops/Iteration#2.js
@@ -6,26 +6,32 @@ false en consecuencia. Una vez lo tengas compruébalo
 con un ***console.log***. */
 
 //1.bucle para recorrer los valores de un objeto dentro de array ¿FOR IN?
-//2.si 2T true entonces 
+//2.si 2T true entonces
 //3.añadir propiedad isApproved = true
 
-
-
-
 const alumns = [
-    {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
-    {name: 'Lucia Aranda', T1: true, T2: false, T3: true},
-    {name: 'Juan Miranda', T1: false, T2: true, T3: true},
-    {name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
-    {name: 'Raquel Benito', T1: true, T2: true, T3: true}
-]
+  { name: "Pepe Viruela", T1: false, T2: false, T3: true },
+  { name: "Lucia Aranda", T1: true, T2: false, T3: true },
+  { name: "Juan Miranda", T1: false, T2: true, T3: true },
+  { name: "Alfredo Blanco", T1: false, T2: false, T3: false },
+  { name: "Raquel Benito", T1: true, T2: true, T3: true },
+];
 
-for (i = 0; i < alumns.length; i++);
-if (alums[i[T1]] == true && alumns[i[T2]] == true & alumns[i[T3]] == true
-    || alums[i[T1]] == true && alumns[i[T2]] == false & alumns[i[T3]] == true
-    || alums[i[T1]] == false && alumns[i[T2]] == true & alumns[i[T3]] == true
-    || alums[i[T1]] == true && alumns[i[T2]] == true & alumns[i[T3]] == false) {
-        console.log(alumns[i] `$isApproved`);
+/* for (let i = 0; i < alumns.length; i++) {
+
+if (alumns[i].T1 && alumns[i].T2
+    || alumns[i].T1 && alumns[i]["T3"]
+    || alumns[i].T2 && alumns[i].T3) {
+        alumns[i].isApproved = true;
     } else {
-        console.log(alumns[i] `$isAnF`);
-    } 
+        alumns[i].isApproved =false;
+    }
+}
+    console.log(alumns); */
+
+for (let alumn of alumns) {
+  (alumn.T1 && alumn.T2) || (alumn.T1 && alumn.T3) || (alumn.T2 && alumn.T3)
+    ? (alumn.isApproved = true)
+    : (alumn.isApproved = false);
+}
+console.log(alumns);
